a88e25af34557fac7148582a2a321f2ee46c7a6c
Max <mcferrenmax@gmail.com>
2019-08-02 11:19:46 -0400

    Updates

---
+++ precache-manifest.0255c75fa6e2ba8fa5cd58ff1c39e8ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5525efc04811cabf33764456e5e521e7",
+    "url": "/hooks_calc/index.html"
+  },
+  {
+    "revision": "ccc79cf4414daf9fffc2",
+    "url": "/hooks_calc/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "bddfa9c3c479d7960afa",
+    "url": "/hooks_calc/static/js/2.5ab775a8.chunk.js"
+  },
+  {
+    "revision": "ccc79cf4414daf9fffc2",
+    "url": "/hooks_calc/static/js/main.2b13e84b.chunk.js"
+  },
+  {
+    "revision": "1169a3bdee5fe6c8ad16",
+    "url": "/hooks_calc/static/js/runtime~main.2ded8e46.js"
+  }
+]);
